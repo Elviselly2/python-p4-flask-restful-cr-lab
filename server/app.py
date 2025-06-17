732425758ad47cbd2b32bf452240cbeec1a7d50c
nosmall <elvisotieno24@gmail.com>
2025-06-17 15:49:04 +0300

    adds content

--- server/app.py
+++ server/app.py
@@ -17,11 +17,33 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return [plant.to_dict() for plant in plants], 200
 
-class PlantByID(Resource):
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(
+            name=data["name"],
+            image=data["image"],
+            price=data["price"]
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        return new_plant.to_dict(), 201
+
+
     pass
-        
+api.add_resource(Plants, "/plants")
+class PlantByID(Resource):
+    def get(self,id):
+        plant = db.session.get(Plant, id)
+        if plant:
+            return plant.to_dict(), 200
+        return {"error": "Plant not found"}, 404
+
+    pass
+api.add_resource(PlantByID, "/plants/<int:id>")      
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
